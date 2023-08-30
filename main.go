4417497218a7e22e407f1acacc3b6d2e2a16ceb0
KingDaemonX <adetoyeanointing15@gmail.com>
2023-08-30 11:11:59 +0100

    deserializing using unmarshal function

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"log/slog"
 	"net/http"
 	"os"
 )
 
+type CatFact struct {
+	Fact   string `json:"fact"`
+	Length int    `json:"length"`
+}
+
 func main() {
+	var catFact CatFact
 	// create a logger type
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	logger.Info("Hello Terminal From Your Favourite Neighbourhood Backend Developer")
@@ -23,7 +30,16 @@ func main() {
 
 	defer resp.Body.Close()
 
-	body, _ := io.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		logger.Error("error occured while reading response body data into variable", "error", err.Error())
+		return
+	}
 
-	logger.Info(string(body), "user", os.Getenv("USER"))
+	if err := json.Unmarshal(body, &catFact); err != nil {
+		logger.Error("error occured while marshalling data into catFact struct", "error", err.Error())
+		return
+	}
+
+	logger.Info(catFact.Fact, "Fact length", catFact.Length)
 }
